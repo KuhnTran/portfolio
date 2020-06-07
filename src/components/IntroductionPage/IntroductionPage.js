b672eb5b02097270f3f6dbd8d84ee2cbba3dff82
KuhnTran <KuhnTran@gmail.com>
2020-06-06 20:55:38 -0700

    added responsiveness to website

--- src/components/IntroductionPage/IntroductionPage.js
+++ src/components/IntroductionPage/IntroductionPage.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import './IntroductionPage.css';
+import './IntroductionPage.scss';
 
 const IntroductionPage = (props) =>
 {
-    const [hovering, setHovering] = React.useState(false);
     return(
         <div style={{ minHeight: '100vh' }} 
-            className="flex flex-column justify-center items-start pl7 mid-gray">
+            className="flex flex-column 
+                mid-gray text-wrapper"
+            id='text-wrapper'>
             <div className='flex flex-row'>
                 <h1>Hello, my name is&nbsp;</h1>
-                <div className='gradient-text'>
-                    <h1>Khoa</h1>
+                <div className='theme-color'>
+                    <h1>Harry Khoa</h1>
                 </div>
             </div>
             <h1>I am the Developer you need</h1>
-            <button className={'b pa3 bg-white ba grow br3 b12 hover-transition'
-                + ((hovering) ? ' gradient-background' : ' gradient-text')
-                }
-                id='contact-button'
-                onMouseEnter={()=>{setHovering(true);}}
-                onMouseLeave={()=>{setHovering(false);}}>Contact Me</button>
+            <button className='b mv3 pa3 bg-white ba pointer 
+                br3 b12 theme-color'>
+                <p className='ma0 pa0'>Contact Me</p>
+            </button>
         </div>
     )
 }
