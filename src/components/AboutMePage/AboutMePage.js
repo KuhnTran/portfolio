e9e6359d285de994160e10d7f2601ce182143cf1
KuhnTran <KuhnTran@gmail.com>
2020-06-07 20:43:26 -0700

    added Project Page structure

--- src/components/AboutMePage/AboutMePage.js
+++ src/components/AboutMePage/AboutMePage.js
@@ -6,7 +6,7 @@ const AboutMePage = (props) =>
 {
     return (
         <div id='about-wrapper' className='flex flex-column items-center white'>
-            <h1 className='pa4'>About Me</h1>
+            <h1 className='pa4'>ABOUT ME</h1>
             <div className="flex flex-row justify-center items-center">
                 <img className='pa4'src={profilePicture} alt='profile_picture' width='300' height ='auto'></img>
                 <p className='pa4'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.
--- src/components/ProjectsPage/ProjectPage.js
+++ src/components/ProjectsPage/ProjectPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import facial_recognition from './../../assets/facial_recognition.png'
 
 const ProjectsPage = (props) =>
 {
     return (
-        <div></div>
+        <div>
+            <div className='flex flex-column items-center mid-gray'>
+                <h1>PROJECTS</h1>
+                <div className='flex flex-row'>
+                    
+                    <div className='flex flex-column'>
+                        <h2>Facial Recognition Webapp</h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.</p>
+                        <div className='flex flex-row'>
+                            <button>Live Demo</button>
+                            <button>Source Code</button>
+                        </div>
+                    </div>
+                    <img src={facial_recognition} alt='Facial Recognition App'></img>
+                </div>
+            </div>
+        </div>
     )
 }
 
