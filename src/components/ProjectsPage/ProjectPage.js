050efdc5724a69772fb5761f9d19f55edf60962f
KuhnTran <KuhnTran@gmail.com>
2020-06-08 20:41:38 -0700

    added Project Page with responsiveness

--- src/components/ProjectsPage/ProjectPage.js
+++ src/components/ProjectsPage/ProjectPage.js
@@ -5,19 +5,20 @@ const ProjectsPage = (props) =>
 {
     return (
         <div>
-            <div className='flex flex-column items-center mid-gray'>
+            <div className='mt4 flex flex-column items-center mid-gray'>
                 <h1>PROJECTS</h1>
-                <div className='flex flex-row ma4'>
-                    
-                    <div className='flex flex-column ma4'>
+                <div id='project-content' className='ma4'>
+                    <div className='flex flex-column'>
                         <h2>Facial Recognition WebApp</h2>
-                        <p style={{width: 600,}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.</p>
+                        <p id='description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.</p>
                         <div className='flex flex-row'>
-                            <button className='whiteBackground ba'>Live Demo</button>
-                            <button className='grow'>Source Code</button>
+                            <button className='whiteBackground ba pa3 b br3 ma1 pointer'>Live Demo</button>
+                            <button id='source-button' className='grow b ma1 pointer'>Source Code</button>
                         </div>
                     </div>
-                    <img className='shadow-3 grow' src={facial_recognition} alt='Facial Recognition App' height='400' width='auto'></img>
+                    <div className='flex flex-column justify-center items-center'>
+                    <img id='project-img' className='shadow-3 grow' src={facial_recognition} alt='Facial Recognition App' width='700' heigh='auto'></img>
+                    </div>
                 </div>
             </div>
         </div>
