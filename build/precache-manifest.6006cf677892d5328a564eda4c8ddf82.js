f557add3261f0dd78fe64616d6d97fc9f831f5a4
KuhnTran <KuhnTran@gmail.com>
2020-06-10 17:08:48 -0700

    deploy to github pages

---
+++ build/precache-manifest.6006cf677892d5328a564eda4c8ddf82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08534fcf4a057cc0444df4a1b038a0a3",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "cf2f5b9fe3eff6999b66",
+    "url": "/portfolio-website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "89723273f2d91a2d582b",
+    "url": "/portfolio-website/static/css/main.585c00d1.chunk.css"
+  },
+  {
+    "revision": "cf2f5b9fe3eff6999b66",
+    "url": "/portfolio-website/static/js/2.95244214.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-website/static/js/2.95244214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89723273f2d91a2d582b",
+    "url": "/portfolio-website/static/js/main.f43cc8f3.chunk.js"
+  },
+  {
+    "revision": "39183fe88caab2a8ff79",
+    "url": "/portfolio-website/static/js/runtime-main.fa61377d.js"
+  },
+  {
+    "revision": "3fe95b4bf4b5b3189493706343f39f57",
+    "url": "/portfolio-website/static/media/facial_recognition.3fe95b4b.png"
+  },
+  {
+    "revision": "69540908626696775836d844578f8015",
+    "url": "/portfolio-website/static/media/github-logo-white.69540908.png"
+  },
+  {
+    "revision": "3b920b22bb6f0cf6b442bf1c964adc89",
+    "url": "/portfolio-website/static/media/profilePicture.3b920b22.jpg"
+  },
+  {
+    "revision": "0388b0751608a5a464b37aeca755d882",
+    "url": "/portfolio-website/static/media/top-white.0388b075.png"
+  }
+]);
