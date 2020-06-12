2f6a2e7394700b2fe5b0a89abe53ae4aed89ba09
KuhnTran <KuhnTran@gmail.com>
2020-06-11 19:14:00 -0700

    post-deployment

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7dc6c7ca6c67efc8c289671d31b6b7b3.js"
+  "/portfolio/precache-manifest.14916016b744bdcaea84f2da74dac6cd.js"
 );
 
 self.addEventListener('message', (event) => {
